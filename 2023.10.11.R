f3bc47567158e765fc6c5e96f9ed085d9c154959
Stanleyismeeeeee <144879190+Stanleyismeeeeee@users.noreply.github.com>
2023-10-11 14:59:22 +0800

    Add files via upload

---
+++ 2023.10.11.R
@@ -0,0 +1,58 @@
+#my economic grades
+
+myEconomicGrades <- c("john","Mary")
+
+my_Economic_Grades <- c("john","Mary")
+
+#import json file----
+
+jsonlite::fromJSON("data/international_flights.json")
+
+##data information ----
+data1 <- list(
+  file = "data/international_flights.json",
+  meta = list(
+    name = "國際航空定期時刻表",
+    source_link = "https://data.gov.tw/dataset/161167")
+)
+
+
+data1$file 
+data1$meta$name 
+data1$meta$source_link
+
+data1 <- list(
+  file = "data/international_flights.json",
+  meta = list(
+    name = "國際航空定期時刻表",
+    source_link = "https://data.gov.tw/dataset/161167")
+)
+
+
+flights$data[[1]]# Give me the first data's information 
+
+flights <- list(
+  data = list(data1) # vector of your 
+)
+data1$filepath
+data1$title 
+data1$
+
+saveRDS(something, ...)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+flights$data[[1]]$data_frame 
+
